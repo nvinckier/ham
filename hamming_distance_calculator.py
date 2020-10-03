a2f18c36128c00a32c52783952688b4b86220aee
nvinckier <44171738+nvinckier@users.noreply.github.com>
2020-10-02 17:42:21 -0700

    Update hamming_distance_calculator.py

--- hamming_distance_calculator.py
+++ hamming_distance_calculator.py
@@ -1,12 +1,64 @@
 #!/usr/bin/python3
 
+import sys
+import getopt
 import pandas as pd
 import numpy as np
 import os
-import matplotlib.pyplot as plt
+
+# Command line arguments
+sample_sheet_file = None    # -i
+hamming_distance_maximum = 3    # -d
+
+# process_args - Process command line arguments and update globals variables.
+
+def process_args():
+    global sample_sheet_file, hamming_distance_maximum
+
+    if len(sys.argv) <= 1:
+        usage()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:d:h")
+    except Exception:
+        usage()
+        sys.exit(1)
+
+    for o, a in opts:
+        if o == "-i":
+            sample_sheet_file = a
+        if o == "-d":
+            hamming_distance_maximum = a
+        if o == "-h":
+            usage()
+
+def usage():
+    print("")
+    print("                                 )\\   /|")
+    print("                              .-/'-|_/ |")
+    print("           __            __,-' (   / \\/")
+    print("       .-'\"  \"'-..__,-'\"\"          -o.`-._")
+    print("      /                                   (\")")
+    print("*--._ ./      Hamming                  ___.-'")
+    print("    |         Distance             _.-' ")
+    print("    :         Calculator        .-/   ")
+    print("     \\        0.2.0          )_ /")
+    print("      \\                _)   / \\(")
+    print("        `.   /-.___.---'(  /   \\\\ ")
+    print("         (  /   \\\\       \\(     L\\ ")
+    print("          \\(     L\\       \\\\ ")
+    print("           \\\\              \\\\ ")
+    print("            L\\              L\\ ")
+    print("")
+    print("Usage: hamming_distance_calculcator.py [Options] -i <SampleSheet CSV File>")
+    print("")
+    print("[Options]")
+    print("    -d <string>                Maximum hamming distance to report. Default = 3")
+    print("    -i <path/to/SampleSheet>   Sample sheet expected in standard CSV format with [Data] section")
+    print("")
+    sys.exit(1)
 
 # Sample sheet opening and parsing function
-
 def open_SS(file):
     df=""
     num=""
@@ -78,29 +130,51 @@ def hamming_distance(string1, string2):
     # Return the final count of differences
     return distance
 
-# Request for sample sheet filename
+####################################################################################################
+# main
+#
+def main():
+    global sample_sheet_file, hamming_distance_maximum
+    process_args()
+    print("Reporting indexes with hamming distance of %s or less" % hamming_distance_maximum)
+    if str(type(sample_sheet_file)) == "<class 'NoneType'>":
+        sample_sheet_file = sys.argv[len(sys.argv) - 1]
+        print("Sample sheet is: " + str(sample_sheet_file))
+    if not os.path.exists(sample_sheet_file):
+        print("ERROR: Sample sheet %s does not exist" % sample_sheet_file)
+        usage()
 
-# sampleSheet = "C:\\Users\\nvinckier\\OneDrive\\scripts\\barcode\\SampleSheet.csv"
-# sampleSheet = "C:\\Users\\nvinckier\\OneDrive\\scripts\\barcode\\test.csv"
-sampleSheet = ""
-sampleSheet = input('Please input a file name:\n')
-if sampleSheet == "":
-	sampleSheet = "SampleSheet.csv"
-indexSeries=""
-indexSeries=open_SS(sampleSheet)
-indexList=list(indexSeries)
-items=len(indexList)
-# print(items)
-hamDistList=pd.DataFrame({'First index string':[],'Second index string':[],'Hamming Distance':[]})
-for num1 in range(0,items):
-#     print(num1)
-    str1=indexList[num1]
-    for num2 in range(0,items):
-        if num1 != num2:
-            str2=indexList[num2]
-            hamDist=hamming_distance(str1,str2)
-            newRow = {'First index string':str1, 'Second index string':str2, 'Hamming Distance':hamDist}
-            hamDistList = hamDistList.append(newRow, ignore_index=True)
-pd.set_option('precision',0)
-print(hamDistList)
-#             print('The Hamming distance between ' + str1.upper() + ' and ' + str2.upper() + ' is: ' + str(hamDist))
+    indexSeries=""
+    indexSeries=open_SS(sample_sheet_file)
+    indexList=list(indexSeries)
+    items=len(indexList)
+    # print(items)
+    hamDistList=pd.DataFrame({'First index string':[],'Second index string':[],'Hamming Distance':[]})
+    for num1 in range(0,items):
+    #     print(num1)
+        str1=indexList[num1]
+        for num2 in range(0,items):
+            if num1 != num2:
+                str2=indexList[num2]
+                # print(str1 + "\t" + str2)
+                hamDist=hamming_distance(str1,str2)
+                if 0 < hamDist <= int(hamming_distance_maximum):
+                    newRow = {'First index string':str1, 'Second index string':str2, 'Hamming Distance':hamDist}
+                    hamDistList = hamDistList.append(newRow, ignore_index=True)
+                elif hamDist == 0:
+                	newRow = {'First index string':str1, 'Second index string':str2, 'Hamming Distance':"Barcode Collision!"}
+                	hamDistList = hamDistList.append(newRow, ignore_index=True)
+    pd.set_option('precision',0)
+    if len(hamDistList) == 0:
+        print()
+        print("No indexes found with hamming distance of %s or less in %s" % (hamming_distance_maximum, sample_sheet_file))
+    else:
+        hamDistListStyled = hamDistList.style.set_properties(**{'text-align': 'left'})
+        hamDistListStyled.set_table_styles([dict(selector='th', props=[('text-align', 'left')])])
+        hamDistList = hamDistList.to_string(index = False)
+        print()
+        print(hamDistList)
+    return hamDistList
+
+if __name__ == "__main__":
+    main()
